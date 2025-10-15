c2290f72690812958c8371bec45820ebfeab391b
Suren <kumar.s.suren@gmail.com>
2025-10-14 22:55:15 -0400

    Add ux

---
+++ ux.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from wm import encode, decode
+
+with gr.Blocks() as app:
+    with gr.Row():
+        with gr.Column(scale=1):
+            input_func = gr.Textbox(placeholder="input function", lines=5, max_lines=5, show_label=False, container=False)
+            payload = gr.Textbox(placeholder="payload", lines=1, max_lines=1, show_label=False, container=False)
+            btn_watermark = gr.Button("watermark")
+        with gr.Column(scale=1):
+            output_func = gr.Textbox(placeholder="watermarked function", lines=5, max_lines=5, show_label=False, container=False)
+            detected_payload = gr.Textbox(placeholder="detected payload", lines=1, max_lines=1, show_label=False, container=False)
+            btn_detect = gr.Button("detect")
+    
+    btn_watermark.click(lambda x, y: encode(x, int(y)), [input_func, payload], output_func)
+    btn_detect.click(lambda x: str(decode(x)), output_func, detected_payload)
+
+app.launch()
---
+++ wm.py
@@ -0,0 +1,7 @@
+def encode(inputText, payload):
+    print(f'encode: txt {inputText}, payload {payload}')
+    return
+
+def decode(inputText):
+    print(f'decode: txt {inputText}')
+    return
